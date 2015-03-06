d71d54e68f08f6774cbd7bc17cf126d3c6b34720
thorhildurha <thorhildurha@gmail.com>
2015-03-06 11:20:45 +0000

    Útlitið semi tilbúið í NewUserForm

--- Login.java
+++ Login.java
@@ -10,7 +10,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-public class Login {
+public class Login extends JFrame {
 	public static void main(String[] args) {
 //		new Login().loginDialog();
 		new Login().NewUserForm();
@@ -27,38 +27,16 @@ public class Login {
 	
 	public void NewUserForm() {
 		JFrame frame = new JFrame("New user application");
-		frame.setSize(600, 600);
+		frame.setSize(400, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		placeNewUserComponents(frame);
 		frame.setVisible(true);
 		
 	}
 	
-//	public static void NewUserForm() {
-//		String[] labels = {"Name: ", "Fax: ", "Email: ", "Address: "};
-//		int numPairs = labels.length;
-//
-//		//Create and populate the panel.
-//		JPanel p = new JPanel(new SpringLayout());
-//		for (int i = 0; i < numPairs; i++) {
-//		    JLabel l = new JLabel(labels[i], JLabel.TRAILING);
-//		    p.add(l);
-//		    JTextField textField = new JTextField(10);
-//		    l.setLabelFor(textField);
-//		    p.add(textField);
-//		}
-//
-//		//Lay out the panel.
-//		SpringUtilities.makeCompactGrid(p,
-//		                                numPairs, 2, //rows, cols
-//		                                6, 6,        //initX, initY
-//		                                6, 6);       //xPad, yPad
-//	}
-//	
-	
 	private static void placeNewUserComponents(JFrame frame) {
 		frame.setLayout(null);
-
+	
 		JLabel nameLabel = new JLabel("Name *");
 		nameLabel.setBounds(10, 10, 80, 25);
 		frame.add(nameLabel);
@@ -91,22 +69,29 @@ public class Login {
 		phoneText.setBounds(100, 100, 160, 25);
 		frame.add(phoneText);
 		
+		JLabel usernameLabel = new JLabel("Username *");
+		usernameLabel.setBounds(10, 130, 80, 25);
+		frame.add(usernameLabel);
 		
-//		JLabel passwordLabel = new JLabel("Password");
-//		passwordLabel.setBounds(10, 40, 80, 25);
-//		frame.add(passwordLabel);
-//
-//		JPasswordField passwordText = new JPasswordField(20);
-//		passwordText.setBounds(100, 40, 160, 25);
-//		frame.add(passwordText);
-//
-//		JButton loginButton = new JButton("login");
-//		loginButton.setBounds(10, 80, 80, 25);
-//		frame.add(loginButton);
-//
-//		JButton registerButton = new JButton("register");
-//		registerButton.setBounds(180, 80, 80, 25);
-//		frame.add(registerButton);
+		JTextField userText = new JTextField(20);
+		userText.setBounds(100, 130, 160, 25);
+		frame.add(userText);
+		
+		JLabel passwordLabel = new JLabel("Password *");
+		passwordLabel.setBounds(10, 160, 80, 25);
+		frame.add(passwordLabel);
+
+		JPasswordField passwordText = new JPasswordField(20);
+		passwordText.setBounds(100, 160, 160, 25);
+		frame.add(passwordText);
+
+		JButton submitButton = new JButton("submit");
+		submitButton.setBounds(10, 200, 80, 25);
+		frame.add(submitButton);
+
+		JButton cancelButton = new JButton("cancel");
+		cancelButton.setBounds(180, 200, 80, 25);
+		frame.add(cancelButton);
 
 	}
 	
