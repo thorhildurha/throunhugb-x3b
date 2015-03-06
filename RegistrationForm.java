c4b2bf1af3b4dc341d5067d58b1ea7e8e98ac1dc
GudrunErla <gudrunerlao@gmail.com>
2015-03-06 21:17:32 +0000

    vuhu

--- RegistrationForm.java
+++ RegistrationForm.java
@@ -7,6 +7,7 @@ import javax.swing.JLabel;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JComboBox;
 
 import java.awt.Container;
 
@@ -14,72 +15,101 @@ import javax.swing.JComponent;
 import javax.swing.GroupLayout;
 
 public class RegistrationForm extends JFrame{
-	private Book registerbook;
+	private Book registerbook; //The book to register
+	private JFrame frame; //The frame for the program, we certainly 
+						 //do not want the size to change depending on which form is open
 	
-	public RegistrationForm(Book x){
+	public RegistrationForm(Book x, JFrame frame){
 		this.registerbook=x;
+		this.frame=frame;
 		initUI();	
 	}
 	private void initUI(){
-		JFrame frame = new JFrame("Registration Form");
-		frame.setSize(300,400);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Registration Form"); // Set a new title to the frame
+		//Create elements
 		JPanel buttonpanel=new JPanel();
 		JPanel labelpane=new JPanel();
-		JLabel price=new JLabel("Price");
-		JLabel condition = new JLabel("Condition");
+		JPanel center=new JPanel();
+		BoxLayout centering=new BoxLayout(center,BoxLayout.Y_AXIS);
+		center.setLayout(centering);
+		
+		JLabel NameLabel= new JLabel("Name: ");
+		JLabel NameVal=new JLabel(this.registerbook.Name);
+		JLabel AuthorLabel=new JLabel("Author: ");
+		JLabel AuthorVal = new JLabel(this.registerbook.Author);
+		JLabel IsbnLabel=new JLabel("ISBN: ");
+		JLabel IsbnVal=new JLabel(this.registerbook.ISBN);
+		JLabel price=new JLabel("Price:");
+		JLabel conditionLabel = new JLabel("Condition:");
 		JTextField pricefield=new JTextField();
-		JTextField conditionField=new JTextField(3);
-		JLabel BookName= new JLabel("Name: ");
-		JLabel name=new JLabel(this.registerbook.Name);
+		
+		String[] conditions={"like new","very good","good","fair","bad","very bad"};
+		JComboBox conditionField=new JComboBox(conditions);
+		
+		//Create Groups for Labels and values/inputfields
 		GroupLayout inputs=new GroupLayout(labelpane);
 		labelpane.setLayout(inputs);
 		inputs.setAutoCreateGaps(true);
+		//First Sequential Group
 		GroupLayout.SequentialGroup hGroup = inputs.createSequentialGroup();
-		GroupLayout.ParallelGroup labels=inputs.createParallelGroup();
-		GroupLayout.ParallelGroup fields=inputs.createParallelGroup();
-		labels.addComponent(BookName);
+		GroupLayout.ParallelGroup labels=inputs.createParallelGroup(); //One for Labels
+		GroupLayout.ParallelGroup fields=inputs.createParallelGroup(); //Other for values/fields
+		labels.addComponent(IsbnLabel);
+		labels.addComponent(NameLabel);
+		labels.addComponent(AuthorLabel);
 		labels.addComponent(price);
-		labels.addComponent(condition);
-		fields.addComponent(name);
+		labels.addComponent(conditionLabel);
+		fields.addComponent(IsbnVal);
+		fields.addComponent(NameVal);
+		fields.addComponent(AuthorVal);
 		fields.addComponent(pricefield,GroupLayout.DEFAULT_SIZE,100,GroupLayout.PREFERRED_SIZE);
-		fields.addComponent(conditionField);
+		fields.addComponent(conditionField,GroupLayout.DEFAULT_SIZE,100,GroupLayout.PREFERRED_SIZE);
 		hGroup.addGroup(labels);
 		hGroup.addGroup(fields);
+		//Then horizontal Group
 		inputs.setHorizontalGroup(hGroup);
 		GroupLayout.SequentialGroup vGroup = inputs.createSequentialGroup();
-		GroupLayout.ParallelGroup priceGroup=inputs.createParallelGroup();
-		GroupLayout.ParallelGroup conditionGroup=inputs.createParallelGroup();
-		GroupLayout.ParallelGroup nameGroup= inputs.createParallelGroup();
-		nameGroup.addComponent(BookName);
-		nameGroup.addComponent(name);
+		
+		GroupLayout.ParallelGroup IsbnGroup=inputs.createParallelGroup(GroupLayout.Alignment.CENTER);
+		GroupLayout.ParallelGroup NameGroup=inputs.createParallelGroup(GroupLayout.Alignment.CENTER);
+		GroupLayout.ParallelGroup AuthorGroup=inputs.createParallelGroup(GroupLayout.Alignment.CENTER);
+		GroupLayout.ParallelGroup priceGroup=inputs.createParallelGroup(GroupLayout.Alignment.CENTER);
+		GroupLayout.ParallelGroup conditionGroup=inputs.createParallelGroup(GroupLayout.Alignment.CENTER);
+		IsbnGroup.addComponent(IsbnLabel);
+		IsbnGroup.addComponent(IsbnVal);
+		NameGroup.addComponent(NameLabel);
+		NameGroup.addComponent(NameVal);
+		AuthorGroup.addComponent(AuthorLabel);
+		AuthorGroup.addComponent(AuthorVal);
 		priceGroup.addComponent(price);
 		priceGroup.addComponent(pricefield, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE);		
-		conditionGroup.addComponent(condition);
+		conditionGroup.addComponent(conditionLabel);
 		conditionGroup.addComponent(conditionField,GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE);
+		vGroup.addGroup(IsbnGroup);
+		vGroup.addGroup(NameGroup);
+		vGroup.addGroup(AuthorGroup);
 		vGroup.addGroup(priceGroup);
 		vGroup.addGroup(conditionGroup);
-		vGroup.addGroup(nameGroup);
 		inputs.setVerticalGroup(vGroup);
+		
 		buttonpanel.setSize(50, 30);
 		JButton register=new JButton("Register");
 		buttonpanel.add(register);
-		JLabel AuthorName=new JLabel("Author: " + this.registerbook.Author);
-		JLabel ISBN=new JLabel("ISBN: " + this.registerbook.ISBN);
-		labelpane.add(ISBN);
-		labelpane.add(BookName);
-		labelpane.add(AuthorName);
-		frame.add(labelpane);
+		center.add(labelpane);
+		frame.add(center);
 		frame.add(buttonpanel,BorderLayout.PAGE_END);
 		frame.setVisible(true);
 	}
 	public static void main(String[] args){
 		Book register=new Book("Litla gula hænan","Andrés Pétursson","02024");
+		JFrame frame = new JFrame("Your app");
+		frame.setSize(300,200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		EventQueue.invokeLater(new Runnable() {
 	        
             @Override
             public void run() {
-                RegistrationForm ex = new RegistrationForm(register);
+                RegistrationForm ex = new RegistrationForm(register,frame);
             }
         });
 	}
