5ea49f5b137b08e9bef0d024aff6cc86ba874807
GudrunErla <gudrunerlao@gmail.com>
2015-03-08 16:40:33 +0000

    :(

---
+++ LoginOwnerTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class LoginOwnerTest {
+	
+	private Owner user;
+	private Login login;
+	private Database database;
+	@Before
+	
+	public void setUp() throws Exception {
+		database= new MockDatabase();
+		login=new Login(user,database);
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		user=null;
+		login=null;
+	}
+
+	@Test
+	public void test() {
+		login.authenticate("dude", "test");
+		assertEquals(user.getUsername(),"dude");
+	}
+
+}
